e629a503bfe5e1f039a7bde748bcfd074a00bfca
Matthias Queitsch <matthias.queitsch@mailbox.org>
2024-03-21 23:12:25 +0100

    initial commit

---
+++ default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.pip
+  ];
+
+  deps = {nixpkgs, ...}: {
+    python = nixpkgs.python310;
+  };
+
+  inherit (pyproject.project) name version;
+
+  mkDerivation = {
+    src = ./.;
+  };
+
+  buildPythonPackage = {
+    format = lib.mkForce "pyproject";
+  };
+
+  pip = {
+    # pypiSnapshotDate = "2023-08-27";
+    requirementsList =
+      pyproject.build-system.requires
+      or []
+      ++ pyproject.project.dependencies;
+    flattenDependencies = true;
+  };
+}
